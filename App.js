1c71a7402613f2e9f7d2a97df4f346f75ca076cd
Hakan <s.hakanayata@gmail.com>
2023-12-11 22:28:51 +0100

    web compatibility for ss added

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
 import CircleButton from './components/CircleButton';
@@ -12,6 +12,7 @@ import * as ImagePicker from 'expo-image-picker'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as MediaLibrary from 'expo-media-library'
 import { captureRef } from 'react-native-view-shot'
+import domtoimage from 'dom-to-image';
 
 const PlaceholderImage = require('./assets/images/background-image.png')
 
@@ -56,17 +57,36 @@ export default function App() {
   }
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      })
-      await MediaLibrary.saveToLibraryAsync(localUri)
-      if (localUri) {
-        alert("Saved!")
+    // on iOS & Android
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        })
+        await MediaLibrary.saveToLibraryAsync(localUri)
+        if (localUri) {
+          alert("Saved!")
+        }
+      } catch (e) {
+        console.log(e)
       }
-    } catch (e) {
-      console.log(e)
+      // on web
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        })
+        let link = document.createElement('a')
+        link.download = 'sticker-smash.jpeg'
+        link.href = dataUrl
+        link.click()
+      } catch (e) {
+        console.log(e)
+
+      }
     }
   }
 
